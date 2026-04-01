2a46e987aa5da82002f2c2e30ca14e1a901cc0de
Farhan Ahmad <110779877+far93878@users.noreply.github.com>
2026-04-01 23:12:54 +0500

    Update settings.py

--- chatbot_api/chatbot_api/settings.py
+++ chatbot_api/chatbot_api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    ''chatbot_api.chatbot'',
+    'chatbot_api.chatbot',
     'rest_framework',
     'corsheaders',
     
